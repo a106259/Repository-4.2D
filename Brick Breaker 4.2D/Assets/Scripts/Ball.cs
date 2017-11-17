ef035cb6e5a0ef9b74c5214a2edd4b3cb7b2cb85
Matteo Micallef <a106259@mcast.org.mt>
2017-11-17 14:20:32 +0100

    Updated Game

---
+++ Brick Breaker 4.2D/Assets/Scripts/Ball.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    Paddle paddle;
+    Vector3 paddleBallDiff;
+    bool gameStart = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        paddle = GameObject.FindObjectOfType<Paddle>();
+        paddleBallDiff = this.transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!gameStart)
+        {
+            this.transform.position = paddle.transform.position + paddleBallDiff;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !gameStart)
+        {
+            gameStart = true;
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameStart) //if(gameStarted ==True)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
